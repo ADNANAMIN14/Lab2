a5805d30521b05d9f36e08d858454e68c7a9d747
ADNANAMIN14 <120156904+ADNANAMIN14@users.noreply.github.com>
2022-12-21 19:23:24 +0500

    PF Tasks

---
+++ Task7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+cout<< "  -------------------------------------       "<<endl;
+cout<< "           ^  ^                               "<<endl;
+cout<< "      o    ____                               "<<endl;
+cout<< "          ( oo ) \\____________________        "<<endl;
+cout<< "       o                                      "<<endl;
+cout<< "          (____) \\                    )\\/\\    "<<endl;
+cout<< "                 ||----------------||         "<<endl;
+cout<< "                 ||                ||         "<<endl;
+cout<< "                 ||                ||         "<<endl;
+cout<< "                 ||                ||         "<<endl;
+cout<< "                                              "<<endl;
+}
